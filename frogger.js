8c849a2f37152f1348d2a180e62e81f39fdd29db
Adrian Zawadzki <wwo00529.az@gmail.com>
2019-05-22 13:16:36 +0200

    [FROGGER-7]  Newpath class player

--- frogger.js
+++ frogger.js
@@ -1,15 +1,14 @@
 const TILE_SIZE=32;
 
+class Player{
+    constructor(){
+        this.model=document.createElement('canvas');
+        this.ctx=this.model.getContext('2d');
+    }
+}
 class Frogger {
     constructor(){}
 
     build(){};
     render(){};
-}
-class Player{
-     constructor(){
-         this.model=document.createElement('canvas');
-         this.ctx=this.model.getContext('2d');
-     }
-
 }
